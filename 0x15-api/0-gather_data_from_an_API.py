b4290675c2affd05371daff22f5f56763c21ca47
Craggy-sense <memusirobi@gmail.com>
2024-08-03 19:16:54 -0700

    inital commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
+
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(
-                    url, user)).json()
-            alltsk = len(jreq)
-            completedtsk = []
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+
+    # Fetch user data
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    employee_name = user.get("name")
+
+    # Fetch TODO list data
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    # Get list of completed tasks
+    completed_tasks = [task.get("title") for task in todos if task.get("completed")]
+
+    # Output format
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, len(completed_tasks), len(todos)))
+    for task_title in completed_tasks:
+        print("\t {}".format(task_title))
+
