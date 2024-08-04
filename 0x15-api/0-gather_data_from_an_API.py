8665fb6ef8dd841a214fde9dc67ed9b565d56b68
Craggy-sense <memusirobi@gmail.com>
2024-08-03 19:05:58 -0700

    inital commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,55 +1,14 @@
 #!/usr/bin/python3
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-def get_employee_data(employee_id):
-    """Fetch employee data from the REST API"""
-    try:
-        #Fetch user data
-        user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-
-        # Fetch TODO list data
-        todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
-
-        return user_data, todos_data
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None, None
-
-def main(employee_id):
-    """Main function to display employee TODO list progress."""
-    user_data, todos_data = get_employee_data(employee_id)
-
-    if not user_data or not todos_data:
-        print("No data found.")
-        return
-
-    employee_name = user_data.get('name')
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get('completed')]
-    number_of_done_tasks = len(done_tasks)
-
-    # Print the result
-    print(f"Employee {employee_name} is done with tasks"
-          f"({number_of_done_tasks}/{total_tasks}):")
-
-    for task in done_tasks:
-        print(f"\t {task.get('title')}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: todo_list.py <employee_id>")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    try:
-        employee_id = int(sys.argv[1])
-        main(employee_id)
-    except ValueError:
-        print("Please provide a valid employee ID as an integer.")
-        sys.exit(1)
-
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
